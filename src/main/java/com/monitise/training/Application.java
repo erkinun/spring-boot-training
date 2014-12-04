53f612284eff6dfda2f757c4c2511b150a52e40d
Erkin Unlu <erkin.unlu@monitise.com>
2014-12-04 16:30:57 +0200

    added bean names

--- src/main/java/com/monitise/training/Application.java
+++ src/main/java/com/monitise/training/Application.java
@@ -2,8 +2,14 @@ package com.monitise.training;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Created by unlue on 04/12/14.
@@ -14,6 +20,17 @@ import org.springframework.context.annotation.Configuration;
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+
+        System.out.println("printing bean names: ");
+
+        List<String> beanListSorted = Arrays.stream(ctx.getBeanDefinitionNames())
+                .sorted()
+                .map(bean -> bean.toString())
+                .collect(Collectors.toList());
+
+        for (String name : beanListSorted) {
+            System.out.println(name);
+        }
     }
 }
