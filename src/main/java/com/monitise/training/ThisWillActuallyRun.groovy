9d63c71ab7d04819281654a3046a7a7907adc883
Erkin Unlu <erkin.unlu@monitise.com>
2014-12-05 15:33:57 +0200

    we can also run groovy=====================

---
+++ src/main/java/com/monitise/training/ThisWillActuallyRun.groovy
@@ -0,0 +1,14 @@
+package com.monitise.training
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class ThisWillActuallyRun {
+
+    @RequestMapping("/hello")
+    String home() {
+        return "Hello World!"
+    }
+
+}
