edc71db13091bdc9e3d43656ff97065a82b6ee55
Erkin Unlu <erkin.unlu@monitise.com>
2014-12-12 15:41:21 +0200

    simple MVC example =======================

---
+++ src/main/java/com/monitise/training/controllers/MvcExampleController.java
@@ -0,0 +1,26 @@
+package com.monitise.training.controllers;
+
+import com.monitise.training.model.BaseModel;
+import com.monitise.training.model.Greeter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by unlue on 12/12/14.
+ */
+@RestController
+public class MvcExampleController {
+
+    @RequestMapping(value = "mvc", method = RequestMethod.GET)
+    public Object getOrElse(@RequestParam("number") int number) {
+
+        if (number < 10) {
+            return new Greeter("hede", "hodo");
+        }
+        else {
+            return BaseModel.failure();
+        }
+    }
+}
