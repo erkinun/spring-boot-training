07a070f0974adb271801b3c7032d619c58e1085d
Erkin Unlu <erkin.unlu@monitise.com>
2014-12-14 18:52:19 +0200

    using Java 8 Optional with Spring 4.1=======================

--- src/main/java/com/monitise/training/controllers/EmployeeController.java
+++ src/main/java/com/monitise/training/controllers/EmployeeController.java
@@ -5,9 +5,11 @@ import com.monitise.training.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by unlue on 09/12/14.
@@ -19,7 +21,10 @@ public class EmployeeController {
     private EmployeeService employeeService;
 
     @RequestMapping(value = "employee", method = RequestMethod.GET)
-    public List<Employee> getEmployees() {
-        return employeeService.getEmployeesByAge(0);
+    public List<Employee> getEmployees(@RequestParam Optional<Integer> age) {
+
+        int param = age.orElse(0);
+
+        return employeeService.getEmployeesByAge(param);
     }
 }
