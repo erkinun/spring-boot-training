b72f0e891da43bf119a2cc14a21c6be64fedb27d
Erkin Unlu <erkin.unlu@monitise.com>
2014-12-14 21:58:56 +0200

    added lazy to employee service

--- src/main/java/com/monitise/training/controllers/EmployeeController.java
+++ src/main/java/com/monitise/training/controllers/EmployeeController.java
@@ -3,6 +3,7 @@ package com.monitise.training.controllers;
 import com.monitise.training.model.Employee;
 import com.monitise.training.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,6 +19,7 @@ import java.util.Optional;
 public class EmployeeController {
 
     @Autowired
+    @Lazy
     private EmployeeService employeeService;
 
     @RequestMapping(value = "employee", method = RequestMethod.GET)
